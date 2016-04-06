8bc0c252468726cf4ea32de9931cc331ac164f03
superkojiman <superkojiman@techorganic.com>
2016-04-06 19:52:55 -0400

    Change container suffix from "pwnbox" to "ctf".

--- run.sh
+++ run.sh
@@ -18,7 +18,7 @@ fi
 ctf_name=${1}
 
 # set default docker-machine env ctf if available
-if [[ ! -z `which docker-machine ctf` ]]; then
+if [[ ! -z `which docker-machine` ]]; then
     docker-machine env ctf > /dev/null 2>&1
     if [[ $? -eq 0 ]]; then
         eval `docker-machine env ctf`
@@ -31,9 +31,9 @@ fi
 # Create docker container and run in the background
 docker run -it \
     -d \
-    -h ${ctf_name}-pwnbox \
+    -h ${ctf_name}-ctf \
     --security-opt seccomp:unconfined \
-    --name ${ctf_name}-pwnbox \
+    --name ${ctf_name}-ctf \
     superkojiman/pwnbox
 
 # Tar config files in rc and extract it into the container
@@ -48,7 +48,7 @@ if [[ -d rc ]]; then
         fi
     done
     cd - > /dev/null 2>&1
-    cat rc/rc.tar | docker cp - ${ctf_name}-pwnbox:/root/
+    cat rc/rc.tar | docker cp - ${ctf_name}-ctf:/root/
 else
     echo -e "${RED}No rc directory found. Nothing to copy to container.${RESET}"
 fi
@@ -56,15 +56,15 @@ fi
 # Create stop/rm script for container
 cat << EOF > ${ctf_name}-stop.sh
 #!/bin/bash
-docker stop ${ctf_name}-pwnbox
-docker rm ${ctf_name}-pwnbox
+docker stop ${ctf_name}-ctf
+docker rm ${ctf_name}-ctf
 rm -f ${ctf_name}-stop.sh
 EOF
 chmod 755 ${ctf_name}-stop.sh
 
 # Create a workdir for this CTF
-docker exec ${ctf_name}-pwnbox mkdir /root/work
+docker exec ${ctf_name}-ctf mkdir /root/work
 
 # Get a shell
 echo -e "${GREEN}Let's pwn stuff!${RESET}"
-docker attach ${ctf_name}-pwnbox
+docker attach ${ctf_name}-ctf
